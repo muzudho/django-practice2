f80df3ecda6db36bd0d23d5cad0e11f31188b1a3
むずでょ <muzudho1@gmail.com>
2022-06-28 20:14:22 +0900

    apps.py 変更

--- host1/apps1/practice/apps.py
+++ host1/apps1/practice/apps.py
@@ -3,4 +3,11 @@ from django.apps import AppConfig
 
 class PracticeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'practice'
+    # * 変更前
+    #name = 'practice'
+    # * 変更後
+    name = 'apps1.practice'
+    #       --------------
+    #       1
+    # 1. `host1/apps1/practice/apps.py`
+    #           --------------
