f182f00a14eb8851bb72b3763901b90d511d5ea4
むずでょ <muzudho1@gmail.com>
2022-07-16 09:42:02 +0900

    整理

--- host1/apps1/practice/views/v0o0o1/room/v_read.py
+++ host1/apps1/practice/views/v0o0o1/room/v_read.py
@@ -1,22 +1,22 @@
-import json
-from django.core import serializers
 from django.shortcuts import render
 
 from apps1.practice.models.v0o0o1.m_room import Room
-#    ----- -------- ------------- ------        ----
-#    1     2        3             4             5
-# 1,3. ディレクトリー名
-# 2. アプリケーション フォルダー名
-# 4. Python ファイル名。拡張子抜き
-# 5. クラス名
+#          --------               ------        ----
+#          1.1                    1.2           2
+#    -----------------------------------
+#    1
+# 1, 1.2 ディレクトリー
+# 1.1 アプリケーション
+# 2. `1.2` に含まれる __init__.py ファイルにさらに含まれるクラス
 
 from apps1.practice.models_helper.mh_user import MhUser
-#    ----- -------- ------------- -------        ------
-#    1     2        3             4              5
-# 1,3. ディレクトリー名
-# 2. アプリケーション フォルダー名
-# 4. Python ファイル名。拡張子抜き
-# 5. クラス名
+#          --------               -------        ------
+#          1.1                    1.2            2
+#    ------------------------------------
+#    1
+# 1, 1.2 ディレクトリー
+# 1.1 アプリケーション
+# 2. `1.2` に含まれる __init__.py ファイルにさらに含まれるクラス
 
 
 @staticmethod
@@ -25,12 +25,10 @@ def render_read(request, room_pk, path_of_read_page):
 
     # ２段階変換: 問合せ結果（QuerySet） ----> JSON文字列 ----> オブジェクト
     # idを指定してメンバーを１人取得
-    room_table_qs = Room.objects.filter(pk=room_pk)  # QuerySet
-    room_table_json = serializers.serialize('json', room_table_qs)
-    room_table_doc = json.loads(room_table_json)  # オブジェクト
+    room_resultset = Room.objects.filter(pk=room_pk)
 
     # 使いやすい形に変換します
-    if len(room_table_doc) < 1:
+    if len(room_resultset) < 1:
         room_dic = {
             "pk": room_pk,
             "name": "",
@@ -43,23 +41,23 @@ def render_read(request, room_pk, path_of_read_page):
         }
     else:
         # 先頭の１件
-        room_rec = room_table_doc[0]
+        room = room_resultset[0]
 
-        sente_id = room_rec["fields"]["sente_id"]
-        gote_id = room_rec["fields"]["gote_id"]
+        sente_id = room.sente_id
+        gote_id = room.gote_id
 
         room_dic = {
-            "pk": room_rec["pk"],
-            "name": room_rec["fields"]["name"],
+            "pk": room.pk,
+            "name": room.name,
             "sente_id": sente_id,
             "sente_name": MhUser.get_name_by_pk(sente_id),
             "gote_id": gote_id,
             "gote_name": MhUser.get_name_by_pk(gote_id),
-            "board": room_rec["fields"]["board"],
-            "record": room_rec["fields"]["record"],
+            "board": room.board,
+            "record": room.record,
         }
 
     context = {
-        'room': room_dic,  # room_table_qs,
+        'room': room_dic,
     }
     return render(request, path_of_read_page, context)
