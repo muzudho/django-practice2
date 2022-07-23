aeae3891e3a33d1ae885d595e29f2d6d22680041
むずでょ <muzudho1@gmail.com>
2022-07-23 15:35:09 +0900

    active use list

--- host1/apps1/practice_v1/views/o1o0/session/v_render.py
+++ host1/apps1/practice_v1/views/o1o0/session/v_render.py
@@ -1,20 +1,19 @@
 import json
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 # セッション モデルヘルパー
 from apps1.practice_v1.models_helper.o1o0.mh_session import MhSession
-#    ----- ----------- -----------------------------        ---------
-#    1     2           3                                    4
-# 1,3. ディレクトリー名
-# 2. アプリケーション フォルダー名
-# 4. クラス名
+#          -----------                    ----------        ---------
+#          11                             12                2
+#    -----------------------------------------------
+#    10
+# 10, 12. ディレクトリー
+# 11. アプリケーション
+# 2. クラス
 
 
 def render_active_user_list(request, path_of_this_page):
     """描画 - アクティブ ユーザー一覧"""
-
-    template = loader.get_template(path_of_this_page)
 
     context = {
         # * `dj_` - 「Djangoがレンダーに埋め込む変数」 の目印
@@ -22,4 +21,4 @@ def render_active_user_list(request, path_of_this_page):
         'dj_users': json.dumps(MhSession.get_all_logged_in_users())
     }
 
-    return HttpResponse(template.render(context, request))
+    return render(request, path_of_this_page, context)
