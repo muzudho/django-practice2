7c798b67aa88ad9369f4d8457e6c8ff41c43fbcf
むずでょ <muzudho1@gmail.com>
2022-07-31 23:39:04 +0900

    Brush up

--- src1/apps1/tic_tac_toe_v1/views/o1o0/match_application/v_render.py
+++ src1/apps1/tic_tac_toe_v1/views/o1o0/match_application/v_render.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, redirect
 
 
-def render_match_application(request, path_of_http_playing, path_of_html):
-    """描画 - 対局申込"""
+def render_match_application(request, upf_playing, lp_match_application):
+    """OA16o1o0gA14o0 描画 - 対局申込
+
+    Parameters
+    ----------
+    upf_playing : str
+        Url Path Format
+    lp_match_application : str
+        Local Path
+    """
 
     if request.method == "POST":
         # 送信後
         room_name = request.POST.get("room_name")
         myPiece = request.POST.get("my_piece")
         # TODO バリデーションチェックしたい
-        return redirect(path_of_http_playing.format(room_name, myPiece))
+        return redirect(upf_playing.format(room_name, myPiece))
 
     # 訪問後
-    return render(request, path_of_html, {})
+    return render(request, lp_match_application, {})
