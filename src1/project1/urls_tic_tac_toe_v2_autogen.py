e48928b4f08e10ea64cd60032ed338a05cc7dd31
むずでょ <muzudho1@gmail.com>
2022-08-21 23:17:24 +0900

    URL autogen

--- src1/project1/urls_tic_tac_toe_v2_autogen.py
+++ src1/project1/urls_tic_tac_toe_v2_autogen.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 from apps1.tic_tac_toe_v2.views.think.engine_manual.v1o0 import EngineManual
 from apps1.tic_tac_toe_v2.views.msg.c2s_json_gen.v1o0 import C2sJsonGenView as C2sJsonGenViewV1o0
+from apps1.tic_tac_toe_v2.views.msg.s2c_json_gen.v1o0 import S2cJsonGenView as S2cJsonGenViewV1o0
 
 
 urlpatterns = [
@@ -12,6 +13,9 @@ urlpatterns = [
 
     # OA16o3o_1o0g4o1o0 〇×ゲーム v2 C2S JSON ジェネレーター
     path('tic-tac-toe/v2/c2s-json-gen/', C2sJsonGenViewV1o0.render, name='practice_v1_vuetify_textarea1_to_model'),
+
+    # OA16o3o_2o0g5o1o0 〇×ゲーム v2 S2C JSON ジェネレーター
+    path('tic-tac-toe/v2/s2c-json-gen/', S2cJsonGenViewV1o0.render, name='practice_v1_vuetify_textarea1_to_model'),
 ]
 
 # EOF O3o2o_1o0g4o0
