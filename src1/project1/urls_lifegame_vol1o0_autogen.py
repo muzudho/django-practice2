9046ef6448e278f58b71c303ed72c4f1422f5898
むずでょ <muzudho1@gmail.com>
2022-08-26 23:34:44 +0900

    Data更新

--- src1/project1/urls_lifegame_vol1o0_autogen.py
+++ src1/project1/urls_lifegame_vol1o0_autogen.py
@@ -8,13 +8,13 @@ from apps1.lifegame_vol1o0.views.board.ver0o3o0 import BoardView as Lifegame1o0B
 
 
 urlpatterns = [
-    # OAAA1001o1o0ga10o1o0 ライフゲーム v0.1 の盤
+    # OAAA1001o1o0ga10o1o0 ライフゲーム1.0巻 盤0.1版
     path('lifegame/vol1.0/board/ver0.1', Lifegame1o0BoardView0o1o0.render),
 
-    # OAAA1001o1o0ga12o_7o1o0 ライフゲーム v0.2 の盤
+    # OAAA1001o1o0ga12o_7o1o0 ライフゲーム1.0巻 盤0.2版
     path('lifegame/vol1.0/board/ver0.2', Lifegame1o0BoardView0o2o0.render),
 
-    # OAAA1001o1o0ga15o1o0 ライフゲーム v0.3 の盤
+    # OAAA1001o1o0ga15o1o0 ライフゲーム1.0巻 盤0.3版
     path('lifegame/vol1.0/board/ver0.3', Lifegame1o0BoardView0o3o0.render),
 ]
 
--- src1/project1/urls_tic_tac_toe_vol2o0_autogen.py
+++ src1/project1/urls_tic_tac_toe_vol2o0_autogen.py
@@ -10,19 +10,19 @@ from apps1.tic_tac_toe_vol2o0.views.gui.playing.ver1o0 import PlayingV
 
 
 urlpatterns = [
-    # OA16o2o0gA16o1o0 〇×ゲーム v2 思考エンジン手動
+    # OA16o2o0gA16o1o0 〇×ゲーム2.0巻 思考エンジン手動1.0版
     path('tic-tac-toe/vol2.0/engine-manual/ver1.0/', EngineManual.render),
 
-    # OA16o3o_1o0g4o1o0 〇×ゲーム v2 C2S JSON ジェネレーター
+    # OA16o3o_1o0g4o1o0 〇×ゲーム2.0巻 C2S JSON ジェネレーター1.0版
     path('tic-tac-toe/vol2.0/c2s-json-gen/ver1.0/', C2sJsonGenViewV1o0.render),
 
-    # OA16o3o_2o0g5o1o0 〇×ゲーム v2 S2C JSON ジェネレーター
+    # OA16o3o_2o0g5o1o0 〇×ゲーム2.0巻 S2C JSON ジェネレーター1.0版
     path('tic-tac-toe/vol2.0/s2c-json-gen/ver1.0/', S2cJsonGenViewV1o0.render),
 
-    # OA16o3o0gA15o1o0 〇×ゲーム v2 対局申込ページ
+    # OA16o3o0gA15o1o0 〇×ゲーム2.0巻 対局申込中1.0版
     path('tic-tac-toe/vol2.0/match-application/ver1.0/', MatchApplicationV.render),
 
-    # OA16o3o0gA15o1o0 〇×ゲーム v2 対局中ページ
+    # OA16o3o0gA15o1o0 〇×ゲーム2.0巻 対局中1.0版
     path('tic-tac-toe/vol2.0/playing/ver1.0/<str:kw_room_name>/', PlayingV.render),
 ]
 
